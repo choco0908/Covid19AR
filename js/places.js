96c04bb85a3e7f11cec107c4d853848762f58c2f
choco0908 <starfish0908@naver.com>
2020-02-25 13:55:34 +0900

    코로나맵 AR 업데이트 ( 2020.02.25 )

--- js/places.js
+++ js/places.js
@@ -1,81 +1,94 @@
 /*
-[{"date": "2/1","place_name": "테스트 장소","type_name": "0번재 확진자","latlng": "{\"lat\":37.499633, \"lng\":127.094559}"}],[{"date": "2/1","place_name": "테스트 장소","type_name": "0번재 확진자","latlng": "{\"lat\":37.517161, \"lng\":127.099494}"}]
+[{date: "2/1",address: "테스트 장소",title: "0번재 확진자",latlng: new naver.maps.LatLng(37.499633,127.094559)}],[{date: "2/1",address: "테스트 장소",title: "0번재 확진자",latlng: new naver.maps.LatLng(37.520757, 127.103117)}],
 */
+
 window.onload = () => {
     const scene = document.querySelector('a-scene');
 
     // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
+    return navigator.geolocation.getCurrentPosition(function (userposition) {
+        console.log("사용자 위치는 : "+userposition.coords.latitude+","+userposition.coords.longitude);
         // then use it to load from remote APIs some places nearby
-        fetch('/js/metadata')
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(myJson) {
-          console.log(JSON.stringify(myJson));
-          return myJson;
-        }).then((people) => {
-                people.forEach((places) => {
-                    places.forEach((place) => {
-                        var latlng = JSON.parse(place.latlng);
+        var people =[];
 
-                        if(!latlng.lat || !latlng.lng ) return true;
+        position.forEach(list => {
+            var list = list.map(single => {
 
-                        const latitude = latlng.lat;
-                        const longitude = latlng.lng;
-                        
-                        //if(computeDistance(position.coords,latitude,longitude) > 1000) return true;
+                return {
+                    month: single.month,
+                    day: single.day,
+                    date: single.date,
+                    place_name: single.address,
+                    type_name: single.title,
+                    full_name: single.full,
+                    latlng: single.latlng
+                }
+                
+            });
+            people.push(list);
+        });
+        console.log(people);
+        people.forEach((places) => {
+            places.forEach((place) => {
+                var latlng = place.latlng;
 
-                        // add place icon
-                        const icon = document.createElement('a-image');
-                        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                        icon.setAttribute('place_name', place.place_name);
-                        if(place.type_name){
-                          icon.setAttribute('type_name', place.type_name);
-                        }
-                        else{
-                          icon.setAttribute('type_name', place.full_name);
-                        }
-                        icon.setAttribute('src', '/assets/map-marker.png');
-                        icon.setAttribute('distance-check','');
+                if(!latlng || !latlng.latitude || !latlng.longitude ) return true;
 
-                        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                        icon.setAttribute('scale', '10 10 10');
+                const latitude = latlng.latitude;
+                const longitude = latlng.longitude;
+                
+                if(computeDistance(userposition.coords,latitude,longitude) > 500) return true;
 
-                        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-                        
-                        const clickListener = function(ev) {
-                            
-                            ev.stopPropagation();
-                            ev.preventDefault();
+                // add place icon
+                const icon = document.createElement('a-image');
+                icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                console.log('gps-entity-place {lat:'+latitude+',lng:'+longitude+'}  loaded');
+                icon.setAttribute('place_name', place.place_name);
+                if(place.type_name){
+                  icon.setAttribute('type_name', place.type_name);
+                }
+                else{
+                  icon.setAttribute('type_name', place.full_name);
+                }
+                icon.setAttribute('src', '/assets/map-marker.png');
+                icon.setAttribute('distance-check','');
 
-                            const type_name = ev.target.getAttribute('type_name');
-                            const place_name = ev.target.getAttribute('place_name');
-                            const dist = ev.target.getAttribute('distance')
-                            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+                // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+                icon.setAttribute('scale', '10 10 10');
 
-                            if (el && el === ev.target) {
-                                const label = document.createElement('span');
-                                const container = document.createElement('div');
-                                container.setAttribute('id', 'place-label');
-                                label.innerText = type_name+" 동선\n"+place_name+"\n"+Math.round(dist)+"m 앞";
-                                container.appendChild(label);
-                                document.body.appendChild(container);
+                icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+                
+                const clickListener = function(ev) {
+                    
+                    ev.stopPropagation();
+                    ev.preventDefault();
 
-                                setTimeout(() => {
-                                    container.parentElement.removeChild(container);
-                                }, 5000);
-                            }
-                        };
+                    const type_name = ev.target.getAttribute('type_name');
+                    const place_name = ev.target.getAttribute('place_name');
+                    const dist = ev.target.getAttribute('distance')
+                    const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-                        icon.addEventListener('click', clickListener);
-                        
-                        scene.appendChild(icon);
-                    });
-                });
-                console.log(people.length+"명 위치 Camera Map 에 표시 완료");
-            })
+                    if (el && el === ev.target) {
+                        const label = document.createElement('span');
+                        const container = document.createElement('div');
+                        container.setAttribute('id', 'place-label');
+                        label.innerText = type_name+" 동선\n"+place_name+"\n"+Math.round(dist)+"m 앞";
+                        container.appendChild(label);
+                        document.body.appendChild(container);
+
+                        setTimeout(() => {
+                            container.parentElement.removeChild(container);
+                        }, 5000);
+                    }
+                };
+
+                icon.addEventListener('click', clickListener);
+                
+                scene.appendChild(icon);
+            });
+        });
+        console.log(people.length+"명 위치 Camera Map 에 표시 완료");
+        
     },
         (err) => console.error('Error in retrieving position', err),
         {
